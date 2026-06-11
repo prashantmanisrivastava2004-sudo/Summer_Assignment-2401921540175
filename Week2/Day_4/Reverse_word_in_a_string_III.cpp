561b9fe8bead75030b24286dcea9cf7d05421ce4
Prashant <prashantmanisrivastava2004@gmail.com>
2026-06-11 11:09:06 +0530

    Day_4(Reverse_word_in_a_string_III)

---
+++ Week2/Day_4/Reverse_word_in_a_string_III.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start =0;
+        for(int i=0; i<=s.size(); i++){
+            if(i==s.size() || s[i]==' '){
+                reverse(s.begin()+start, s.begin()+i);
+                 start = i+1;
+            }
+        }
+        return s;
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
