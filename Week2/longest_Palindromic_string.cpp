c0abb531afa92662c1ee226a4108dbf3d91102d9
Prashant <prashantmanisrivastava2004@gmail.com>
2026-06-12 14:59:28 +0530

    Day_5(Longest_Palindromic_string)

---
+++ Week2/longest_Palindromic_string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int maxLen = 1;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            // Odd length palindrome
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
+
+//Time Complexity: O(n^2)
+//Space Complexity: O(1)
